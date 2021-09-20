d5d9b64ad1a0061fa9a085ae21d346dce785030e
Samarth Reddy <samarthreddyryan@gmail.com>
2021-09-21 02:42:59 +0530

    fixing callback

--- configure/passport.js
+++ configure/passport.js
@@ -38,7 +38,7 @@ module.exports = function(passport) {
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/users/auth/google/callback'
+      callbackURL: 'https://youuchat.herokuapp.com/users/auth/google/callback'
     },
     ( accessToken, refreshToken, profile, done ) => {
 
